99c62bff4005e78a4d782f76521723313351d90a
Stefan Arnold <stefan.arnold@operun.de>
2019-03-18 16:44:29 +0100

    "Add vocabulary: SectionBackgroundColors"

---
+++ src/collective/frontpage/tests/test_vocab_section_background_colors.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from collective.frontpage import _
+from collective.frontpage.testing import COLLECTIVE_FRONTPAGE_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.interfaces import IVocabularyTokenized
+
+import unittest
+
+
+class SectionBackgroundColorsIntegrationTest(unittest.TestCase):
+
+    layer = COLLECTIVE_FRONTPAGE_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_vocab_section_background_colors(self):
+        vocab_name = 'collective.frontpage.SectionBackgroundColors'
+        factory = getUtility(IVocabularyFactory, vocab_name)
+        self.assertTrue(IVocabularyFactory.providedBy(factory))
+
+        vocabulary = factory(self.portal)
+        self.assertTrue(IVocabularyTokenized.providedBy(vocabulary))
+        self.assertEqual(
+            vocabulary.getTerm('sony-a7r-iii').title,
+            _(u'Sony Aplha 7R III'),
+        )
