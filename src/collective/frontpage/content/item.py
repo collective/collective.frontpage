6591fa65a058e72cba1ed2f2a132118996560c93
ArminBeer <armin.beer@gmx.de>
2019-03-19 11:02:46 +0100

    added fields to content types

--- src/collective/frontpage/content/item.py
+++ src/collective/frontpage/content/item.py
@@ -1,57 +1,42 @@
 # -*- coding: utf-8 -*-
+
 from plone.app.textfield import RichText
-from plone.autoform import directives
 from plone.dexterity.content import Item
-from plone.namedfile import field as namedfile
 from plone.supermodel import model
-from plone.supermodel.directives import fieldset
-from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import implementer
+from plone.namedfile.field import NamedBlobImage
 from collective.frontpage import _
 
 
 class IItem(model.Schema):
-    """ Marker interface and Dexterity Python Schema for Item
+    """
+    Marker interface and Dexterity Python Schema for Item.
     """
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    link_url = schema.URI(
+        title=_(u'Link'),
+        required=False,
+    )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    link_title = schema.TextLine(
+        title=_(u'Linktitle'),
+        required=False,
+    )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    icon = schema.TextLine(
+        title=_(u'tile_icon_title', default='Icon'),
+        description=_(u'tile_icon_description',
+                      default=u'Select an available icon.'),
+        required=False,
+    )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    image = NamedBlobImage(
+        title=_(u'Image'),
+        required=False,
+    )
 
 
-@implementer(IItem)
 class Item(Item):
-    """
-    """
+
+    pass
