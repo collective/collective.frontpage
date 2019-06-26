5bd97cbb9ff6c6f2164f45c3f66f8c86cdb3dcf8
Jesse Stippel <jesse.stippel@operun.de>
2019-06-26 10:52:36 +0200

    add sections as contenttypes

--- src/collective/frontpage/browser/section.py
+++ src/collective/frontpage/browser/section.py
@@ -7,7 +7,14 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class SectionView(SectionViewMixin, BrowserView):
 
-    template = ViewPageTemplateFile("templates/section.pt")
+    template = ViewPageTemplateFile('templates/section.pt')
 
     def __call__(self):
+        self.layout = self.context.getLayout()
+        self.contents = self.context.listFolderContents()
         return self.template()
+
+    def is_layout(self, layout):
+        if layout == self.layout:
+            return True
+        return False
