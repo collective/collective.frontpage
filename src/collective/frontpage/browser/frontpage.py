3ebaaa82b1352497afd5041775274ef4d95bf86f
Jesse Stippel <jesse.stippel@operun.de>
2019-06-27 17:22:16 +0200

    add editing tools

--- src/collective/frontpage/browser/frontpage.py
+++ src/collective/frontpage/browser/frontpage.py
@@ -3,6 +3,7 @@
 from lxml import html
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
 
 
 class Frontpage(BrowserView):
@@ -13,10 +14,14 @@ class Frontpage(BrowserView):
         self.section_classname = 'frontpage-section'
         return self.template()
 
+    def is_anonymous(self):
+        return api.user.is_anonymous()
+
     def get_sections(self):
         contents = self.context.listFolderContents()
         section_list = list()
         for section in contents:
+            section_url = section.absolute_url()
             section_view = section.unrestrictedTraverse(section.getLayout())
             section_view.request.set('ajax_load', 'True')
             tree = html.fromstring(section_view())
@@ -26,5 +31,11 @@ class Frontpage(BrowserView):
                 )
             )
             for elem in parsed_html:
-                section_list.append(html.tostring(elem))
+                section_list.append(
+                    {
+                        'url': section_url,
+                        'html': html.tostring(elem),
+                        'style': section_view.get_style(),
+                    }
+                )
         return section_list
